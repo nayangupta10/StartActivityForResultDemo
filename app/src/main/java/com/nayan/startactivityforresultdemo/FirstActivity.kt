917121af6b29aa4531ddf973b1047320ce1ac9d6
Nayan's <nayanguptaa10@gmail.com>
2021-06-10 21:20:18 +0530

    initial commit

---
+++ app/src/main/java/com/nayan/startactivityforresultdemo/FirstActivity.kt
@@ -0,0 +1,18 @@
+package com.nayan.startactivityforresultdemo
+
+import android.app.Activity
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_first.*
+
+class FirstActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_first)
+
+        btnFinish.setOnClickListener {
+            setResult(Activity.RESULT_OK)
+            finish()
+        }
+    }
+}
